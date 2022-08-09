311c1a702ccceb98a82de4908685ad02d1cbd2fa
SJ <seongjun.kim@deepspaceshipping.co>
2022-08-09 12:09:45 -0700

    WARP-118: wrapper around RG

--- lib/raygun.dart
+++ lib/raygun.dart
@@ -14,12 +14,26 @@ class Message {
   late String id;
   late String conversationId;
   late String senderId;
-  late int date;
-  // Bool pinned
-  // List<Reactions> reactions
-  // UUID replied
+  late DateTime date;
+  late bool pinned;
+  late List<Reaction> reactions;
+  late String replied;
   late List<String> value;
-  // Map<String, String> metadata
+  late Map<String, String> metadata;
+}
+
+class Reaction {
+  late String emoji;
+  late List<String> senderId;
+}
+
+enum Uid {
+  id,
+  didKey,
+}
+
+class SenderId {
+  late Uid uid;
 }
 
 class Raygun {
@@ -40,13 +54,37 @@ class Raygun {
     for (int i = 0; i < length; i++) {
       Message message = Message();
       Pointer<G_Message> msgPointer = result.data.ref.ptr.elementAt(i).value;
-      message.id = bindings.message_id(msgPointer).toString();
-      message.conversationId =
-          bindings.message_conversation_id(msgPointer).toString();
-      message.senderId = bindings.message_sender_id(msgPointer).toString();
 
-      // TODO: How to convert Rust ffi.Int8 to Dart int?
-      // message.date = bindings.message_date(msgPointer);
+      message.id = bindings.message_id(msgPointer).value.toString();
+      message.conversationId =
+          bindings.message_conversation_id(msgPointer).value.toString();
+
+      // TODO: sender ID can be uuid or did with its own class, how to?
+      Pointer<G_SenderId> senderIdPointer =
+          bindings.message_sender_id(msgPointer);
+
+      Pointer<Int8> senderId = bindings.sender_id_get_id(senderIdPointer);
+      if (senderId.value != 0) {
+        message.senderId = senderId.value.toString();
+      } else {
+        Pointer<G_DID> senderDid =
+            bindings.sender_id_get_did_key(senderIdPointer);
+        message.senderId = DID(senderDid).toString();
+      }
+
+      // TODO: Is this the right way to convert Rust ffi.Int8 to Dart DateTime?
+      message.date = DateTime(bindings.message_date(msgPointer).value);
+
+      // TODO: Reactions
+      // - seems like we suppose to use the returned pointer for reaction_emoji and reaction_users
+      // - but the message_reactions returns only 1 pointer. Shouldn't there be a list?
+      // message.reactions =
+      bindings.message_reactions(msgPointer);
+
+      message.pinned = bindings.message_pinned(msgPointer) == 0 ? false : true;
+
+      // TODO: Replied - where is it?
+      // bindings.message_
 
       // Message body
       Pointer<G_FFIVec_String> lines = bindings.message_lines(msgPointer);
@@ -55,6 +93,9 @@ class Raygun {
         message.value.add(lines.ref.ptr.value.toString());
       }
       msgs.add(message);
+
+      // TODO: Metadata - Rust struct has a map, but no related function appears.
+      // bindings.metadata
     }
 
     return msgs;
