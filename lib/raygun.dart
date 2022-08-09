c873f39778fe734207804519c64c1a03fcfa1dcb
SJ <seongjun.kim@deepspaceshipping.co>
2022-08-08 18:22:30 -0700

    chore: intial trials

--- lib/raygun.dart
+++ lib/raygun.dart
@@ -10,11 +10,23 @@ import 'package:flutter/foundation.dart';
 import 'package:warp_dart/warp.dart';
 import 'package:warp_dart/warp_dart_bindings_generated.dart';
 
+class Message {
+  late String id;
+  late String conversationId;
+  late DID senderId;
+  // DateTime date
+  // Bool pinned
+  // List<Reactions> reactions
+  // UUID replied
+  late List<String> value;
+  // Map<String, String> metadata
+}
+
 class Raygun {
   Pointer<G_RayGunAdapter> pointer;
   Raygun(this.pointer);
 
-  List<String> getMessages(String conversationID) {
+  Message getMessages(String conversationID) {
     G_FFIResult_FFIVec_Message result = bindings.raygun_get_messages(
         pointer, conversationID.toNativeUtf8().cast<Int8>());
 
@@ -22,14 +34,14 @@ class Raygun {
       throw WarpException(result.error);
     }
 
-    List<String> list = [];
+    Message message = Message();
     int length = result.data.ref.len;
 
     for (int i = 0; i < length; i++) {
       Pointer<G_Message> pointer = result.data.ref.ptr.elementAt(i).value;
-      list.add(pointer.toString());
+      message.value.add(pointer.toString());
     }
 
-    return list;
+    return message;
   }
 }
