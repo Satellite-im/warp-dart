c2f03a56ea1a38c712e0510b0870b2e7784b69fa
SJ <seongjun.kim@deepspaceshipping.co>
2022-08-15 21:18:35 -0700

    WARP-118: update for a recent breaking change of mp_ipfs

--- lib/mp_ipfs.dart
+++ lib/mp_ipfs.dart
@@ -18,13 +18,10 @@ DynamicLibrary ipfs_dlib = DynamicLibrary.open('lib$_libNameIpfs.so');
 final WarpDartBindings _ipfs_bindings = WarpDartBindings(ipfs_dlib);
 
 MultiPass multipass_ipfs_temporary(Tesseract tesseract) {
-  const String config =
-  //"{\"path\":\"/data/data/com.example.warp_dart_example/files\",\"bootstrap\":[\"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN\"], \"listen_on\":[\"/ip4/0.0.0.0/tcp/0\"],\"ipfs_setting\":{\"mdns\":{\"enable\":true},\"autonat\":{\"enable\":false,\"servers\":[]},\"relay_client\":{\"enable\":false,\"relay_address\":null},\"relay_server\":{\"enable\":false},\"dcutr\":{\"enable\":false},\"rendezvous\":{\"enable\":false,\"address\":\"\"}},\"store_setting\":{\"broadcast_interval\":10,\"broadcast_with_connection\":true, \"discovery\":false}}";
-      "{\"path\":null,\"bootstrap\":[\"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN\"], \"listen_on\":[\"/ip4/0.0.0.0/tcp/0\"],\"ipfs_setting\":{\"mdns\":{\"enable\":true},\"autonat\":{\"enable\":false,\"servers\":[]},\"relay_client\":{\"enable\":false,\"relay_address\":null},\"relay_server\":{\"enable\":false},\"dcutr\":{\"enable\":false},\"rendezvous\":{\"enable\":false,\"address\":\"\"}},\"store_setting\":{\"broadcast_interval\":10,\"broadcast_with_connection\":true, \"discovery\":false}}";
+  Pointer<G_MpIpfsConfig> config = _ipfs_bindings.mp_ipfs_config_testing();
 
   G_FFIResult_MultiPassAdapter result = _ipfs_bindings
-      .multipass_mp_ipfs_temporary(
-      nullptr, tesseract.getPointer(), config.toNativeUtf8().cast<Int8>());
+      .multipass_mp_ipfs_temporary(nullptr, tesseract.getPointer(), config);
 
   if (result.error.address.toString() != "0") {
     throw WarpException(result.error.cast());
@@ -33,20 +30,15 @@ MultiPass multipass_ipfs_temporary(Tesseract tesseract) {
   return MultiPass(result.data);
 }
 
-
 //Note: Before this function is called, we should make sure tesseract
 // - Is unlocked
 // - Has a file set
 // - Has autosave enabled
 MultiPass multipass_ipfs_persistent(Tesseract tesseract, String path) {
-
-  //This will change in the future where configuration object is exported via ffi
-  String config =
-      "{\"path\":\"$path\",\"bootstrap\":[\"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN\"], \"listen_on\":[\"/ip4/0.0.0.0/tcp/0\"],\"ipfs_setting\":{\"mdns\":{\"enable\":true},\"autonat\":{\"enable\":false,\"servers\":[]},\"relay_client\":{\"enable\":false,\"relay_address\":null},\"relay_server\":{\"enable\":false},\"dcutr\":{\"enable\":false},\"rendezvous\":{\"enable\":false,\"address\":\"\"}},\"store_setting\":{\"broadcast_interval\":10,\"broadcast_with_connection\":true, \"discovery\":false}}";
+  Pointer<G_MpIpfsConfig> config = _ipfs_bindings.mp_ipfs_config_development();
 
   G_FFIResult_MultiPassAdapter result = _ipfs_bindings
-      .multipass_mp_ipfs_persistent(
-      nullptr, tesseract.getPointer(), config.toNativeUtf8().cast<Int8>());
+      .multipass_mp_ipfs_persistent(nullptr, tesseract.getPointer(), config);
 
   if (result.error.address.toString() != "0") {
     throw WarpException(result.error.cast());
